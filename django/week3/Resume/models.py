391aa4d56f9a8c0fbebafb8c07d20e31b258063d
Viraj <salvi.viraj007@gmail.com>
2018-02-19 13:15:49 -0500

    adding experience model in model.py

--- django/week3/Resume/models.py
+++ django/week3/Resume/models.py
@@ -10,3 +10,10 @@ class Education (models.Model):
     degree = models.CharField(max_length=64, null=False, blank=False)
     major = models.CharField(max_length=64, null=False, blank=False)
     gpa = models.FloatField(null=False, blank=False)
+
+class Experience (models.Model):
+    title = models.CharField(max_length=64, null=False, blank=False)
+    location = models.CharField(max_length=64, null=False, blank=False)
+    start_date = models.DateField(auto_now=False, auto_now_add=False, blank=False)
+    end_date = models.DateField(auto_now=False, auto_now_add=False, blank=False)
+    description = models.TextField(max_length=64, null=False, blank=True)
